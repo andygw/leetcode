c3bdbfebf7bc4d276863028303f158f9743cd04b
Andrew Wever <andrew.wever@gmail.com>
2022-12-21 18:02:07 -0500

    Python, convert the temperature

---
+++ Python/convert-the-temperature.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/convert-the-temperature/
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        fahrenheit = celsius * 1.8 + 32
+        kelvin = celsius + 273.15
+
+        return [kelvin, fahrenheit]
