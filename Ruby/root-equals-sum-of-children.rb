e49c8092b2c5bcd10932ddee0d1c7c6c4105c7a2
Andrew Wever <andrew.wever@gmail.com>
2022-12-21 18:00:59 -0500

    Ruby, root equals sum of children

---
+++ Ruby/root-equals-sum-of-children.rb
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/root-equals-sum-of-children/
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Boolean}
+def check_tree(root)
+  root.val == root.left.val + root.right.val
+end
