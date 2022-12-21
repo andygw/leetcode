4f9136195df34fe520260483e07de8e6d2f00004
Andrew Wever <andrew.wever@gmail.com>
2022-12-21 17:17:33 -0500

    Ruby, reverse string

---
+++ Ruby/reverse-string.rb
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/reverse-string/
+
+# @param {Character[]} s
+# @return {Void} Do not return anything, modify s in-place instead.
+def reverse_string(s)
+  s.reverse!
+end
